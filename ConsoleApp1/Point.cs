6239bfc3dfc245574fecad24c72f982fd3362912
GoldenSkull96 <bondar.illya09@gmail.com>
2025-03-11 12:01:28 +0200

    добавлен класс Point

---
+++ ConsoleApp1/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Point
+    {
+        private int x;
+        private int y;
+        private int z;
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+        public Point(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,35 +1,5 @@
 ﻿using Newtonsoft.Json;
-public class Vector
-{
-    private int[] start;
-    private int[] end;
-    private int[] coordinates;
-    private double length;
-    public int[] Start { get { return start; } }
-    public int[] End { get { return end; } }
-    public int[] Coordinates { get { return coordinates; } }
-    public double Length { get { return length; } }
-
-    public Vector(int[] start, int[] end)
-    {
-        this.start = start;
-        this.end = end;
-        Coordinates_();
-    }
-    private void Coordinates_()
-    {
-        int[] coordinates = new int[3];
-        coordinates[0] = end[0] - start[0];
-        coordinates[1] = end[1] - start[1];
-        coordinates[2] = end[2] - start[2];
-        this.coordinates = coordinates;
-    }
-    public void Length_()
-    {
-        this.length = Math.Sqrt(Math.Pow(coordinates[0], 2) + Math.Pow(coordinates[1], 2) + Math.Pow(coordinates[2],2));
-        Console.Write(length);
-    }
-}
+using ConsoleApp1;
  class Program
 {
     static void Main()
