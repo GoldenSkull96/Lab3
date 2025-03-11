d15b5d992237b7c0eff0a08cbaeea12a5689dbea
GoldenSkull96 <bondar.illya09@gmail.com>
2025-03-11 12:10:55 +0200

    переписан метод Vector  под точки описаные классом Point

--- ConsoleApp1/Vector.cs
+++ ConsoleApp1/Vector.cs
@@ -8,34 +8,31 @@ namespace ConsoleApp1
 {
     internal class Vector
     {
-        private int[] start;
-        private int[] end;
-        private int[] coordinates;
+        private Point start;
+        private Point end;
+        private Point coordinates;
         private double length;
-        public int[] Start { get { return start; } }
-        public int[] End { get { return end; } }
-        public int[] Coordinates { get { return coordinates; } }
+        public Point Start { get { return start; } }
+        public Point End { get { return end; } }
+        public Point Coordinates { get { return coordinates; } }
         public double Length { get { return length; } }
 
-        public Vector(int[] start, int[] end)
+        public Vector(Point start, Point end)
         {
             this.start = start;
             this.end = end;
             Coordinates_();
+            Length_();
         }
         private void Coordinates_()
         {
-            int[] coordinates = new int[3];
-            coordinates[0] = end[0] - start[0];
-            coordinates[1] = end[1] - start[1];
-            coordinates[2] = end[2] - start[2];
-            this.coordinates = coordinates;
+            this.coordinates = new Point(End.X - Start.X, End.Y - Start.Y, End.Z - Start.Z);
         }
-        public void Length_()
+        private void Length_()
         {
-            this.length = Math.Sqrt(Math.Pow(coordinates[0], 2) + Math.Pow(coordinates[1], 2) + Math.Pow(coordinates[2], 2));
-            Console.Write(length);
+            this.length = Math.Sqrt(Math.Pow(Coordinates.X, 2) + Math.Pow(Coordinates.Y, 2) + Math.Pow(Coordinates.Z, 2));
         }
+
     }
 }
 
